0c7a09c961e56b0a7e3555b1db7383db480c32f3
ralph.tran <ralphtran.sg@gmail.com>
2025-06-11 22:42:23 +0800

    Base setup of python project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A MCP Server to run user-defined runbook in commandline";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+    # Use flake-utils to generate outputs for common systems
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Import packages for the specific system
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        # Define the Python version to use
+        pythonPkgs = pkgs.python313.pkgs;
+      in {
+        # Development Shell accessible via `nix develop`
+        devShells.default = pkgs.mkShell {
+          name = "mcp-server-runbook-dev";
+          # buildInputs provides packages needed for the shell environment.
+          # We need python and uv ad python dependencies
+          buildInputs = [
+            pythonPkgs.python        # Python 3.13
+            pkgs.uv                  # For dependencies management
+          ];
+          # Run shellHook to create virtual env and install dependencies with uv
+          shellHook = ''
+            uv sync
+            source .venv/bin/activate
+          '';
+        };
+      });
+}
